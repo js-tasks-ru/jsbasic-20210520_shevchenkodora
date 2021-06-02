c05c107af8daff11f9fe45e2b9ff38f6603aebe3
dasha <Shevchenkodora@gmail.com>
2021-06-02 13:53:41 +0300

    add function namify(users), function filterRange(arr, a, b), function camelize(str), function showSalary(users, age), function getMinMax(str)

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,6 @@
 function getMinMax(str) {
-  // ваш код...
+  const arr = str.split(/,| /).filter(item => isFinite(item) === true);
+  const min = Math.min.apply(null, arr);
+  const max = Math.max.apply(null, arr);
+  return {"min": min, "max": max};
 }
