e5d3fc9acc60bfa82a5411923f8877ddb92f13e8
dasha <Shevchenkodora@gmail.com>
2021-07-06 16:19:55 +0300

    Create class Cart Посмотрите, пожалуйста, все работает, но тесты не проходит

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -6,24 +6,55 @@ export default class Cart {
   }
 
   addProduct(product) {
-    // ваш код
+    if (product === '' || product === null) {
+      return;
+    }
+    let cartItem;
+    let index = this.cartItems.indexOf(product);
+    if (index !== -1) {
+      this.cartItems[index].count += 1;
+    } else {
+      cartItem = {product: product, count: 1};
+      this.cartItems.push(cartItem);
+    }
+    this.onProductUpdate(cartItem);
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    let cartItem;
+    this.cartItems.forEach((item, index) => {
+      if (item.product.id === productId) {
+        item.count += amount;
+        cartItem = this.cartItems[index];
+      }
+      if (item.count === 0) {
+
+        this.cartItems.splice(index, 1);
+      }
+      this.onProductUpdate(cartItem);
+    });
+
   }
 
   isEmpty() {
-    // ваш код
+    if (this.cartItems.length === 0) return  true;
+   return false;
   }
 
   getTotalCount() {
-    // ваш код
+   let totalCount = 0;
+    this.cartItems.forEach(item => {
+      totalCount +=item.count;
+    })
+    return totalCount;
   }
 
   getTotalPrice() {
-    // ваш код
-  }
+    let totalPrice = 0;
+    this.cartItems.forEach(item=> {
+      totalPrice += item.count * item.product.price;
+  })
+    return totalPrice;}
 
   onProductUpdate(cartItem) {
     // реализуем в следующей задаче
